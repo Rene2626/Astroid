4f08dd4b04d269a7ea3a80684e1ef2a49024ff6f
reneboemken90@gmx.de <reneboemken90@gmx.de>
2024-09-05 12:31:20 +0200

    collison + bullets

---
+++ circleshape.py
@@ -0,0 +1,32 @@
+import pygame
+
+
+# Base class for game objects
+class CircleShape(pygame.sprite.Sprite):
+    def __init__(self, x, y, radius):
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()
+        self.position = pygame.Vector2(x, y)
+        self.velocity = pygame.Vector2(0, 0)
+        self.radius = radius
+        self.rect = pygame.Rect(x - radius, y - radius, radius * 2, radius * 2)
+
+    def draw(self, screen):
+        # must override
+        pass
+
+    def update(self, dt):
+        # must override
+        self.position += self.velocity * dt
+        self.rect.center = self.position
+        pass
+
+    def collision(self, theother):
+        #theother ist einfach nur der andere, name wird nachher angepasst
+        distance =self.position.distance_to(theother.position) #(asteroid 1 durch self und der andere in der klammer)
+        sum_of_radii = self.radius + theother.radius
+        return distance <= sum_of_radii
+# wenn die distance zwischen den beiden asteroiden kleiner ist als die beiden radii zusammen dann kollidieren die asteroiden
+
